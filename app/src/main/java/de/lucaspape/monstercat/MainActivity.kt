6333a8ec80cc1b44a95d04a73151bd694c6616c9
Lucas Pape <lucas.pape@web.de>
2019-08-01 23:08:35 +0200

    Disable backup

--- app/src/main/java/de/lucaspape/monstercat/MainActivity.kt
+++ app/src/main/java/de/lucaspape/monstercat/MainActivity.kt
@@ -1,7 +1,6 @@
 package de.lucaspape.monstercat
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
