eb3800bf84d86ed6f0884763ddcc034d1879cc73
Lucas Pape <lucas.pape@web.de>
2019-10-14 20:54:43 +0200

    Cleanup MusicPlayer

---
+++ app/src/main/java/de/lucaspape/monstercat/music/ControlUtil.kt
@@ -0,0 +1,201 @@
+package de.lucaspape.monstercat.music
+
+import android.content.Context
+import android.content.IntentFilter
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.ConnectivityManager
+import de.lucaspape.monstercat.database.Song
+import de.lucaspape.monstercat.settings.Settings
+import java.io.File
+import java.io.FileInputStream
+import java.lang.IndexOutOfBoundsException
+
+/**
+ * Music control methods
+ */
+internal fun play() {
+    try {
+        val song = playList[currentSong]
+
+        val settings = Settings(contextReference!!.get()!!)
+        val url = song.getUrl()
+
+        mediaPlayer.stop()
+        mediaPlayer = MediaPlayer()
+
+        val connectivityManager =
+            contextReference!!.get()!!.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+
+        if (!File(url).exists()) {
+            if (wifi != null && !wifi.isConnected && settings.getSetting("streamOverMobile") != "true") {
+                //TODO msg
+                println("DONT STREAM")
+                return
+            } else {
+                mediaPlayer.setDataSource(url)
+            }
+        } else {
+            val fis = FileInputStream(File(url))
+            mediaPlayer.setDataSource(fis.fd)
+        }
+
+        //Prepare the player
+        mediaPlayer.prepareAsync()
+
+        //if mediaPlayer is finished preparing
+        mediaPlayer.setOnPreparedListener {
+
+
+            mediaPlayer.start()
+
+            playing = true
+
+
+            setTitle(song.title)
+
+            startTextAnimation()
+
+            mediaPlayer.setOnCompletionListener {
+                next()
+            }
+
+            startSeekBarUpdate()
+
+            setCover(song)
+
+            setPlayButtonImage()
+
+            showSongNotification(song.title, song.artist, song.coverUrl, true)
+
+        }
+    } catch (e: IndexOutOfBoundsException) {
+
+    }
+}
+
+/**
+ * Stop playback
+ */
+internal fun stop() {
+    playing = false
+
+    setTitle("")
+
+    setPlayButtonImage()
+
+    mediaPlayer.stop()
+}
+
+/**
+ * Pause playback
+ */
+fun pause() {
+    val context = contextReference!!.get()!!
+    val settings = Settings(context)
+    val primaryResolution = settings.getSetting("primaryCoverResolution")
+
+    try {
+        val song = playList[currentSong]
+
+        val title = song.title
+        val artist = song.artist
+        val coverUrl = context.filesDir.toString() + "/" + song.albumId + ".png" + primaryResolution
+
+        mediaPlayer.pause()
+        showSongNotification(title, artist, coverUrl, false)
+
+        setPlayButtonImage()
+
+        playing = false
+        paused = true
+    } catch (e: IndexOutOfBoundsException) {
+
+    }
+}
+
+/**
+ * Resume playback
+ */
+fun resume() {
+    val context = contextReference!!.get()!!
+    val settings = Settings(context)
+    val primaryResolution = settings.getSetting("primaryCoverResolution")
+
+    try {
+        val song = playList[currentSong]
+
+        val title = song.title
+        val artist = song.artist
+        val coverUrl = context.filesDir.toString() + "/" + song.albumId + ".png" + primaryResolution
+
+        val length = mediaPlayer.currentPosition
+
+        if (paused) {
+            mediaPlayer.seekTo(length)
+            mediaPlayer.start()
+
+            paused = false
+
+            val intentFilter = IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
+            context.registerReceiver(NoisyReceiver(), intentFilter)
+        } else {
+            play()
+        }
+
+        showSongNotification(title, artist, coverUrl, true)
+
+        setPlayButtonImage()
+
+        playing = true
+    } catch (e: IndexOutOfBoundsException) {
+
+    }
+}
+
+/**
+ * Next song
+ */
+fun next() {
+    currentSong++
+    play()
+
+}
+
+/**
+ * Previous song
+ */
+fun previous() {
+    if (currentSong != 0) {
+        currentSong--
+
+        play()
+    }
+}
+
+/**
+ * Play song now
+ */
+fun playNow(song: Song) {
+    try {
+        playList.add(currentSong + 1, song)
+        currentSong++
+    } catch (e: IndexOutOfBoundsException) {
+        playList.add(song)
+    }
+
+
+    play()
+}
+
+/**
+ * Toggle pause/play
+ */
+fun toggleMusic() {
+    if (playing) {
+        pause()
+    } else {
+        resume()
+    }
+}
---
+++ app/src/main/java/de/lucaspape/monstercat/music/UIUtil.kt
@@ -0,0 +1,454 @@
+package de.lucaspape.monstercat.music
+
+import android.animation.ValueAnimator
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.ColorDrawable
+import android.media.MediaMetadata
+import android.media.session.PlaybackState
+import android.os.Handler
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import de.lucaspape.monstercat.R
+import de.lucaspape.monstercat.database.Song
+import de.lucaspape.monstercat.settings.Settings
+import java.io.File
+import java.lang.NullPointerException
+import java.lang.ref.WeakReference
+
+var textView1Reference: WeakReference<TextView>? = null
+var textView2Reference: WeakReference<TextView>? = null
+var seekBarReference: WeakReference<SeekBar>? = null
+var barCoverImageReference: WeakReference<ImageView>? = null
+var musicBarReference: WeakReference<androidx.appcompat.widget.Toolbar>? = null
+var playButtonReference: WeakReference<ImageButton>? = null
+
+var fullscreenTextView1Reference: WeakReference<TextView>? = null
+var fullscreenTextView2Reference: WeakReference<TextView>? = null
+var fullscreenSeekBarReference: WeakReference<SeekBar>? = null
+var fullscreenCoverReference: WeakReference<ImageView>? = null
+var fullscreenPlayButtonReference: WeakReference<ImageButton>? = null
+
+/**
+ * UI update methods
+ */
+fun setTextView(newTextView1: TextView, newTextView2: TextView) {
+    try {
+        newTextView1.text = textView1Reference!!.get()!!.text
+        newTextView2.text = textView2Reference!!.get()!!.text
+    } catch (e: NullPointerException) {
+
+    }
+
+    textView1Reference = WeakReference(newTextView1)
+    textView2Reference = WeakReference(newTextView2)
+}
+
+fun setFullscreenTextView(newTextView1: TextView, newTextView2: TextView) {
+    try {
+        newTextView1.text = textView1Reference!!.get()!!.text
+        newTextView2.text = textView2Reference!!.get()!!.text
+    } catch (e: NullPointerException) {
+
+    }
+
+    fullscreenTextView1Reference = WeakReference(newTextView1)
+    fullscreenTextView2Reference = WeakReference(newTextView2)
+}
+
+fun setSeekBar(newSeekBar: SeekBar) {
+    try {
+        newSeekBar.progress = seekBarReference!!.get()!!.progress
+    } catch (e: NullPointerException) {
+
+    }
+
+    newSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+            if (fromUser)
+                mediaPlayer.seekTo(progress)
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar) {
+        }
+
+        override fun onStopTrackingTouch(seekBar: SeekBar) {
+        }
+    })
+
+    seekBarReference = WeakReference(newSeekBar)
+}
+
+fun setFullscreenSeekBar(newSeekBar: SeekBar) {
+    try {
+        newSeekBar.progress = seekBarReference!!.get()!!.progress
+    } catch (e: NullPointerException) {
+
+    }
+
+    newSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+            if (fromUser)
+                mediaPlayer.seekTo(progress)
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar) {
+        }
+
+        override fun onStopTrackingTouch(seekBar: SeekBar) {
+        }
+    })
+
+    fullscreenSeekBarReference = WeakReference(newSeekBar)
+}
+
+fun setBarCoverImageView(newImageView: ImageView) {
+    try {
+        newImageView.setImageDrawable(barCoverImageReference!!.get()!!.drawable)
+    } catch (e: NullPointerException) {
+
+    }
+
+    barCoverImageReference = WeakReference(newImageView)
+}
+
+fun setFullscreenCoverImageView(newImageView: ImageView) {
+    try {
+        newImageView.setImageDrawable(barCoverImageReference!!.get()!!.drawable)
+    } catch (e: NullPointerException) {
+
+    }
+
+    fullscreenCoverReference = WeakReference(newImageView)
+}
+
+fun setMusicBar(newToolbar: androidx.appcompat.widget.Toolbar) {
+    try {
+        newToolbar.setBackgroundColor((musicBarReference!!.get()!!.background as ColorDrawable).color)
+    } catch (e: NullPointerException) {
+
+    }
+    musicBarReference = WeakReference(newToolbar)
+}
+
+fun setPlayButton(newPlayButton: ImageButton) {
+    if (playing) {
+        newPlayButton.setImageDrawable(
+            ContextCompat.getDrawable(
+                contextReference!!.get()!!,
+                R.drawable.ic_pause_white_24dp
+            )
+        )
+
+    } else {
+        newPlayButton.setImageDrawable(
+            ContextCompat.getDrawable(
+                contextReference!!.get()!!,
+                R.drawable.ic_play_arrow_white_24dp
+            )
+        )
+
+    }
+
+    playButtonReference = WeakReference(newPlayButton)
+}
+
+fun setFullscreenPlayButton(newPlayButton: ImageButton) {
+    if (playing) {
+        newPlayButton.setImageDrawable(
+            ContextCompat.getDrawable(
+                contextReference!!.get()!!,
+                R.drawable.ic_pause_black_24dp
+            )
+        )
+
+    } else {
+        newPlayButton.setImageDrawable(
+            ContextCompat.getDrawable(
+                contextReference!!.get()!!,
+                R.drawable.ic_play_arrow_black_24dp
+            )
+        )
+
+    }
+
+    fullscreenPlayButtonReference = WeakReference(newPlayButton)
+}
+
+internal fun setTitle(title:String){
+    try {
+        textView1Reference!!.get()!!.text = title
+    } catch (e: NullPointerException) {
+
+    }
+    try {
+        textView2Reference!!.get()!!.text = title
+    } catch (e: NullPointerException) {
+
+    }
+
+    try {
+        fullscreenTextView1Reference!!.get()!!.text = title
+    } catch (e: NullPointerException) {
+
+    }
+
+    try {
+        fullscreenTextView2Reference!!.get()!!.text = title
+    } catch (e: NullPointerException) {
+
+    }
+}
+
+internal fun startTextAnimation(){
+    val valueAnimator = ValueAnimator.ofFloat(0.0f, 1.0f)
+    valueAnimator.repeatCount = Animation.INFINITE
+    valueAnimator.interpolator = LinearInterpolator()
+    valueAnimator.duration = 9000L
+    valueAnimator.addUpdateListener { animation ->
+        val progress = animation.animatedValue as Float
+
+        var width = 0
+
+
+        try {
+            width = textView1Reference!!.get()!!.width
+        } catch (e: NullPointerException) {
+
+        }
+
+
+        try {
+            width = fullscreenTextView1Reference!!.get()!!.width
+        } catch (e: NullPointerException) {
+
+        }
+
+
+        val translationX = width * progress
+
+        try {
+            textView1Reference!!.get()!!.translationX = translationX
+        } catch (e: NullPointerException) {
+
+        }
+
+        try {
+            textView2Reference!!.get()!!.translationX = translationX - width
+        } catch (e: NullPointerException) {
+        }
+
+        try {
+            fullscreenTextView1Reference!!.get()!!.translationX = translationX
+        } catch (e: NullPointerException) {
+
+        }
+
+        try {
+            fullscreenTextView2Reference!!.get()!!.translationX = translationX - width
+        } catch (e: NullPointerException) {
+
+        }
+    }
+
+    valueAnimator.start()
+}
+
+internal fun startSeekBarUpdate(){
+    val seekBarUpdateHandler = Handler()
+
+    val updateSeekBar = object : Runnable {
+        override fun run() {
+            try {
+                seekBarReference!!.get()!!.max = mediaPlayer.duration
+                seekBarReference!!.get()!!.progress = mediaPlayer.currentPosition
+            } catch (e: NullPointerException) {
+
+            }
+
+            try {
+                fullscreenSeekBarReference!!.get()!!.max = mediaPlayer.duration
+                fullscreenSeekBarReference!!.get()!!.progress = mediaPlayer.currentPosition
+            } catch (e: NullPointerException) {
+
+            }
+            setPlayerState(mediaPlayer.currentPosition.toLong())
+
+            seekBarUpdateHandler.postDelayed(this, 50)
+        }
+    }
+
+    seekBarUpdateHandler.postDelayed(updateSeekBar, 0)
+
+    try {
+        seekBarReference!!.get()!!.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                if (fromUser)
+                    mediaPlayer.seekTo(progress)
+                setPlayerState(progress.toLong())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+    } catch (e: NullPointerException) {
+
+    }
+
+    try {
+        fullscreenSeekBarReference!!.get()!!.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                if (fromUser)
+                    mediaPlayer.seekTo(progress)
+                setPlayerState(progress.toLong())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+    } catch (e: NullPointerException) {
+
+    }
+}
+
+internal fun setCover(song: Song){
+    val settings = Settings(contextReference!!.get()!!)
+    val primaryResolution = settings.getSetting("primaryCoverResolution")
+
+    val coverFile = File(contextReference!!.get()!!.filesDir.toString() + "/" + song.albumId + ".png" + primaryResolution)
+
+    if (coverFile.exists()) {
+        val bitmap = BitmapFactory.decodeFile(coverFile.absolutePath)
+
+        try {
+            barCoverImageReference!!.get()!!.setImageBitmap(bitmap)
+        } catch (e: NullPointerException) {
+
+        }
+
+        try {
+            fullscreenCoverReference!!.get()!!.setImageBitmap(bitmap)
+        } catch (e: NullPointerException) {
+
+        }
+
+        setSongMetadata(song.artist, song.title, bitmap, mediaPlayer.duration.toLong())
+    } else {
+        setSongMetadata(song.artist, song.title, null, mediaPlayer.duration.toLong())
+    }
+}
+
+internal fun setPlayButtonImage(){
+    if(playing){
+        try {
+            playButtonReference!!.get()!!.setImageDrawable(
+                ContextCompat.getDrawable(
+                    contextReference!!.get()!!,
+                    R.drawable.ic_pause_white_24dp
+                )
+            )
+
+        } catch (e: NullPointerException) {
+
+        }
+
+        try {
+            fullscreenPlayButtonReference!!.get()!!.setImageDrawable(
+                ContextCompat.getDrawable(
+                    contextReference!!.get()!!,
+                    R.drawable.ic_pause_black_24dp
+                )
+            )
+
+        } catch (e: NullPointerException) {
+
+        }
+    }else{
+        try {
+            playButtonReference!!.get()!!.setImageDrawable(
+                ContextCompat.getDrawable(
+                    contextReference!!.get()!!,
+                    R.drawable.ic_play_arrow_white_24dp
+                )
+            )
+
+        } catch (e: NullPointerException) {
+
+        }
+
+        try {
+            fullscreenPlayButtonReference!!.get()!!.setImageDrawable(
+                ContextCompat.getDrawable(
+                    contextReference!!.get()!!,
+                    R.drawable.ic_play_arrow_black_24dp
+                )
+            )
+
+        } catch (e: NullPointerException) {
+
+        }
+    }
+}
+
+/**
+ * SetPlayerState
+ */
+internal fun setPlayerState(progress: Long) {
+    val stateBuilder = PlaybackStateCompat.Builder()
+
+    val state: Int = if (playing) {
+        PlaybackState.STATE_PLAYING
+    } else {
+        PlaybackState.STATE_PAUSED
+    }
+
+    stateBuilder.setState(state, progress, 1.0f)
+    stateBuilder.setActions(
+        PlaybackStateCompat.ACTION_PLAY +
+                PlaybackStateCompat.ACTION_PAUSE +
+                PlaybackStateCompat.ACTION_SKIP_TO_NEXT +
+                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS +
+                PlaybackStateCompat.ACTION_STOP +
+                PlaybackStateCompat.ACTION_PLAY_PAUSE +
+                PlaybackStateCompat.ACTION_SEEK_TO +
+                PlaybackStateCompat.ACTION_FAST_FORWARD +
+                PlaybackStateCompat.ACTION_REWIND
+    )
+    mediaSession!!.setPlaybackState(stateBuilder.build())
+}
+
+/**
+ * Set song metadata
+ */
+internal fun setSongMetadata(artist: String, title: String, coverImage: Bitmap?, duration: Long) {
+    val mediaMetadata = MediaMetadataCompat.Builder()
+    mediaMetadata.putString(MediaMetadata.METADATA_KEY_ARTIST, artist)
+    mediaMetadata.putString(MediaMetadata.METADATA_KEY_TITLE, title)
+    mediaMetadata.putLong(MediaMetadata.METADATA_KEY_DURATION, duration)
+    mediaMetadata.putBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART, coverImage)
+    mediaMetadata.putBitmap(MediaMetadata.METADATA_KEY_ART, coverImage)
+    mediaSession!!.setMetadata(mediaMetadata.build())
+}
