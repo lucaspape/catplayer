037976af20ad2a768b312f25f97d697e2999430f
Lucas Pape <lucas.pape@web.de>
2019-12-01 20:58:45 +0100

    Notification channel

--- app/src/main/java/de/lucaspape/monstercat/music/MusicNotification.kt
+++ app/src/main/java/de/lucaspape/monstercat/music/MusicNotification.kt
@@ -1,29 +1,16 @@
 package de.lucaspape.monstercat.music
 
 import android.app.*
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.support.v4.media.session.MediaSessionCompat
-import android.widget.RemoteViews
-import androidx.core.app.NotificationCompat
-import androidx.palette.graphics.Palette
+import android.os.Build
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
-import de.lucaspape.monstercat.R
 
 //notification var
 const val musicChannelID = "Music Notification"
 const val musicNotificationID = 1
-
-private const val NOTIFICATION_PREVIOUS = "de.lucaspape.monstercat.previous"
-private const val NOTIFICATION_DELETE = "de.lucaspape.monstercat.delete"
-private const val NOTIFICATION_PAUSE = "de.lucaspape.monstercat.pause"
-private const val NOTIFICATION_PLAY = "de.lucaspape.monstercat.play"
-private const val NOTIFICATION_NEXT = "de.lucaspape.monstercat.next"
 
 /**
  * Show notification
@@ -35,6 +22,8 @@ internal fun createSongNotification(
     coverLocation: String,
     playing: Boolean
 ) {
+    createNotificationChannel()
+
     val playerNotificationManager = PlayerNotificationManager(contextReference!!.get()!!, musicChannelID, musicNotificationID, object:
         PlayerNotificationManager.MediaDescriptionAdapter{
         override fun getCurrentLargeIcon(
@@ -65,14 +54,21 @@ internal fun createSongNotification(
     playerNotificationManager.setMediaSessionToken(mediaSession!!.sessionToken)
 }
 
-/**
- * Get the dominant color of a bitmap image (use for notification background color)
- */
-fun getDominantColor(bitmap: Bitmap): Int {
-    val swatchesTemp = Palette.from(bitmap).generate().swatches
-    val swatches = ArrayList<Palette.Swatch>(swatchesTemp)
+private fun createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val context = contextReference!!.get()!!
 
-    swatches.sortWith(Comparator { swatch1, swatch2 -> swatch2.population - swatch1.population })
+        val channelName = "Music Notification"
+        val channelDescription = "Handy dandy description"
+        val importance = NotificationManager.IMPORTANCE_LOW
 
-    return if (swatches.size > 0) swatches[0].rgb else Color.WHITE
+        val notificationChannel = NotificationChannel(musicChannelID, channelName, importance)
+
+        notificationChannel.description = channelDescription
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+
+    }
 }
