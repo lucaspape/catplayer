3bff032f9f2288a047bb31a2c334a2778857a7b7
Lucas Pape <lucas.pape@web.de>
2020-01-25 13:54:50 +0100

    Check for download over mobile

--- app/src/main/java/de/lucaspape/monstercat/download/DownloadHandler.kt
+++ app/src/main/java/de/lucaspape/monstercat/download/DownloadHandler.kt
@@ -8,6 +8,7 @@ import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import de.lucaspape.monstercat.R
 import de.lucaspape.monstercat.util.Settings
+import de.lucaspape.monstercat.util.wifiConnected
 import java.io.File
 import java.io.FileOutputStream
 
@@ -32,7 +33,6 @@ fun downloadCoverIntoImageView(
     lowRes: Boolean
 ) {
     val settings = Settings(context)
-
     val resolution = if (!lowRes) {
         settings.getSetting("primaryResolution")
     } else {
@@ -47,34 +47,39 @@ fun downloadCoverIntoImageView(
         val bitmap = BitmapFactory.decodeFile(cacheFile.absolutePath)
         imageView.setImageBitmap(bitmap)
     } else {
-        val picassoTarget = object : com.squareup.picasso.Target {
-            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
-                imageView.setImageDrawable(placeHolderDrawable)
-            }
+        if (wifiConnected(context) == true || settings.getSetting("downloadCoversOverMobile") == "true") {
+            val picassoTarget = object : com.squareup.picasso.Target {
+                override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+                    imageView.setImageDrawable(placeHolderDrawable)
+                }
 
-            override fun onBitmapFailed(errorDrawable: Drawable?) {
-                imageView.setImageDrawable(errorDrawable)
-            }
+                override fun onBitmapFailed(errorDrawable: Drawable?) {
+                    imageView.setImageDrawable(errorDrawable)
+                }
 
-            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
-                imageView.setImageBitmap(bitmap)
+                override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                    imageView.setImageBitmap(bitmap)
 
-                //possible that it changed by this time
-                if (!cacheFile.exists()) {
-                    FileOutputStream(cacheFile).use { out ->
-                        bitmap?.compress(Bitmap.CompressFormat.PNG, 100, out)
+                    //possible that it changed by this time
+                    if (!cacheFile.exists()) {
+                        FileOutputStream(cacheFile).use { out ->
+                            bitmap?.compress(Bitmap.CompressFormat.PNG, 100, out)
+                        }
                     }
                 }
             }
+
+            //to prevent garbage collect
+            targetList.add(picassoTarget)
+
+            Picasso.with(context)
+                .load("$url?image_width=$resolution")
+                .placeholder(Drawable.createFromPath(context.dataDir.toString() + "/fallback.jpg"))
+                .into(picassoTarget)
+        } else {
+            val bitmap = BitmapFactory.decodeFile(context.dataDir.toString() + "/fallback.jpg")
+            imageView.setImageBitmap(bitmap)
         }
-
-        //to prevent garbage collect
-        targetList.add(picassoTarget)
-
-        Picasso.with(context)
-            .load("$url?image_width=$resolution")
-            .placeholder(Drawable.createFromPath(context.dataDir.toString() + "/fallback.jpg"))
-            .into(picassoTarget)
     }
 }
 
@@ -94,24 +99,36 @@ fun downloadCoverIntoBitmap(
 
     val url = context.getString(R.string.trackContentUrl) + "$albumId/cover"
 
-    val picassoTarget = object : com.squareup.picasso.Target {
-        override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
-        }
+    val cacheFile = File(context.cacheDir.toString() + "/$albumId.png.$resolution")
 
-        override fun onBitmapFailed(errorDrawable: Drawable?) {
-        }
+    if (cacheFile.exists()) {
+        val bitmap = BitmapFactory.decodeFile(cacheFile.absolutePath)
+        downloadFinished(bitmap)
+    } else {
+        if (wifiConnected(context) == true || settings.getSetting("downloadCoversOverMobile") == "true") {
+            val picassoTarget = object : com.squareup.picasso.Target {
+                override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+                }
 
-        override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
-            bitmap?.let {
-                downloadFinished(bitmap)
+                override fun onBitmapFailed(errorDrawable: Drawable?) {
+                }
+
+                override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                    bitmap?.let {
+                        downloadFinished(bitmap)
+                    }
+                }
             }
+
+            //prevent garbage collect
+            targetList.add(picassoTarget)
+
+            Picasso.with(context)
+                .load("$url?image_width=$resolution")
+                .into(picassoTarget)
+        } else {
+            val bitmap = BitmapFactory.decodeFile(context.dataDir.toString() + "/fallback.jpg")
+            downloadFinished(bitmap)
         }
     }
-
-    //prevent garbage collect
-    targetList.add(picassoTarget)
-
-    Picasso.with(context)
-        .load("$url?image_width=$resolution")
-        .into(picassoTarget)
 }
