16b15a9068fe82d77a6f16c489cf8abb29bb872d
Lucas Pape <lucas.pape@web.de>
2020-03-31 14:10:24 +0000

    Use jsonRequest

--- app/src/main/java/de/lucaspape/monstercat/request/AuthorizedJsonObjectRequest.kt
+++ app/src/main/java/de/lucaspape/monstercat/request/AuthorizedJsonObjectRequest.kt
@@ -1,19 +1,42 @@
 package de.lucaspape.monstercat.request
 
+import com.android.volley.NetworkResponse
+import com.android.volley.ParseError
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.HttpHeaderParser
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
 import org.json.JSONObject
-import java.nio.charset.Charset
+import java.lang.NullPointerException
 
 class AuthorizedJsonObjectRequest(
     method: Int,
     url: String,
     private val sid: String?,
     private val jsonObject: JSONObject,
-    listener: Response.Listener<String>,
+    listener: Response.Listener<JSONObject>,
     errorListener: Response.ErrorListener
 
-) : StringRequest(method, url, listener, errorListener) {
+) : JsonObjectRequest(method, url, jsonObject, listener, errorListener) {
+
+    override fun parseNetworkResponse(response: NetworkResponse?): Response<JSONObject> {
+        response?.let {
+            val jsonString = String(response.data, charset(HttpHeaderParser.parseCharset(response.headers, JsonRequest.PROTOCOL_CHARSET)))
+
+            val jsonObject = if (jsonString.isEmpty()) {
+                JSONObject()
+            } else {
+                JSONObject(jsonString)
+            }
+
+            return Response.success(
+                jsonObject, HttpHeaderParser.parseCacheHeaders(response)
+            )
+        }
+
+        return Response.error(ParseError(NullPointerException()));
+    }
+
     override fun getHeaders(): Map<String, String> {
         return if (sid != null) {
             val params = HashMap<String, String>()
@@ -22,13 +45,5 @@ class AuthorizedJsonObjectRequest(
         } else {
             super.getHeaders()
         }
-    }
-
-    override fun getBody(): ByteArray {
-        return jsonObject.toString().toByteArray(Charset.forName("UTF-8"))
-    }
-
-    override fun getBodyContentType(): String {
-        return "application/json; charset=utf-8"
     }
 }
